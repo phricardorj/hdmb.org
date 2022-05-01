ea3e4712fd60b161c4495c49679127689e32b08d
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2022-05-01 03:10:22 -0300

    feat: navbar responsive design

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+const btnMobile = document.querySelector("#btn-mobile");
+
+const toggleMenu = (event) => {
+  if (event.type === "touchstart") event.preventDefault();
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("active");
+  const active = menu.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  else event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+};
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
